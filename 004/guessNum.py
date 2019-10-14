de2ed2dec8ca858ad41ec022174e74add472c7fd
kqyang <kqyang0000@163.com>
2019-10-14 11:24:51 +0800

    系统生成随机数

--- 004/guessNum.py
+++ 004/guessNum.py
@@ -1,14 +1,21 @@
+import random
+
+secret = random.randint(1, 10)
+
 print('--------- 游戏开始 ----------')
 inp = input("请输入:")
 inp = int(inp)
-while inp != 8:
-    inp = input("猜错了，请重新输入:")
-    inp = int(inp)
-    if inp == 8:
-        print("you win")
-    else:
-        if inp > 8:
-            print("大了")
-        else:
-            print("小了")
+if inp == secret:
+    print("you win")
+else:
+    while inp != secret:
+        if inp > secret:
+            inp = input("大了，请重新输入:")
+            inp = int(inp)
+        if inp < secret:
+            inp = input("小了，请重新输入:")
+            inp = int(inp)
+        if inp == secret:
+            print("you win")
+            break;
 print('--------- 游戏结束 ----------')
