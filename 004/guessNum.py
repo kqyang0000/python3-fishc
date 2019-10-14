057c8d9e1997d8e98989eede1db88342f9042dda
kqyang <kqyang0000@163.com>
2019-10-14 11:06:49 +0800

    增加while循环执行

--- 004/guessNum.py
+++ 004/guessNum.py
@@ -1,11 +1,14 @@
 print('--------- 游戏开始 ----------')
 inp = input("请输入:")
 inp = int(inp)
-if inp == 8:
-    print("you win")
-else:
-    if inp > 8:
-        print("大了")
+while inp != 8:
+    inp = input("猜错了，请重新输入:")
+    inp = int(inp)
+    if inp == 8:
+        print("you win")
     else:
-        print("小了")
+        if inp > 8:
+            print("大了")
+        else:
+            print("小了")
 print('--------- 游戏结束 ----------')
