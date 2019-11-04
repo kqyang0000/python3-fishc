de5e88eea09014589d91803aa635ffca9991f2bf
kqyang <kqyang0000@163.com>
2019-11-04 22:32:01 +0800

    tkinter 事件绑定

--- 067/tkinter/tk2.py
+++ 067/tkinter/tk2.py
@@ -8,7 +8,27 @@ tk.Label(root, text='密码：').grid(row=1, column=0)
 v1 = tk.StringVar()
 v2 = tk.StringVar()
 
-e1 = tk.Entry(root, textvariable=v1)
+
+def chk(content, reason, name):
+    if e1.get() == 'a':
+        print(content, reason, name)
+        return True
+    else:
+        e1.delete(0, tk.END)
+        return False
+
+
+def fallback():
+    print('我被调用了')
+
+
+# 函数绑定
+regChk = root.register(chk)
+
+# 失去焦点时验证
+# invalidcommand 只有当validatecommand函数返回false时才回被调用
+e1 = tk.Entry(root, textvariable=v1, validate='focusout', validatecommand=(regChk, '%P', '%v', '%W'),
+              invalidcommand=fallback)
 e2 = tk.Entry(root, textvariable=v2, show='*')
 e1.grid(row=0, column=1, padx=10, pady=5)
 e2.grid(row=1, column=1, padx=10, pady=5)
