977047d3acd1a8b3ccefaa1f94664ee69aca7c0f
kqyang <kqyang0000@163.com>
2019-11-04 22:10:38 +0800

    tkinter 密码框

---
+++ 067/tkinter/tk2.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+tk.Label(root, text='账号：').grid(row=0, column=0)
+tk.Label(root, text='密码：').grid(row=1, column=0)
+
+v1 = tk.StringVar()
+v2 = tk.StringVar()
+
+e1 = tk.Entry(root, textvariable=v1)
+e2 = tk.Entry(root, textvariable=v2, show='*')
+e1.grid(row=0, column=1, padx=10, pady=5)
+e2.grid(row=1, column=1, padx=10, pady=5)
+
+
+def show():
+    print('账号: %s' % e1.get())
+    print('密码: %s' % e2.get())
+
+
+tk.Button(root, text='登陆', width=10, command=show).grid(row=2, column=0, sticky='w', padx=10, pady=5)
+tk.Button(root, text='退出', width=10, command=root.quit).grid(row=2, column=1, sticky='e', padx=10, pady=5)
+
+tk.mainloop()
